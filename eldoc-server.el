270ff15f83d2bbef8b89f0521097c60d68f60d14
Xu Chunyang <xuchunyang56@gmail.com>
2020-10-29 02:44:32 +0800

    Add initial front end

--- eldoc-server.el
+++ eldoc-server.el
@@ -33,6 +33,13 @@
 (defvar eldoc-server-host "0.0.0.0")
 (defvar eldoc-server-port 3000)
 
+(defconst eldoc-server--load-dir
+  (file-name-directory
+   (or load-file-name buffer-file-name))
+  "The directory of the package.")
+
+(defvar eldoc-server--server nil)
+
 (defun eldoc-server--end (proc code object)
   (let ((string (encode-coding-string (concat (json-encode object) "\n") 'utf-8)))
     (ws-response-header
@@ -40,8 +47,6 @@
      '("content-type" . "application/json; charset=utf-8")
      (cons "content-length" (string-bytes string)))
     (process-send-string proc string)))
-
-(defvar eldoc-server--server nil)
 
 ;;;###autoload
 (defun eldoc-server-start ()
@@ -54,13 +59,56 @@
   (setq eldoc-server--server
         (ws-start
          `(((:GET . ,(rx bos "/api/describe-symbol")) .
-            eldoc-server--api-describe-symbol))
+            eldoc-server--api-describe-symbol)
+           ((:GET . ,(rx bos "/" eos)) .
+            eldoc-server--index)
+           ((lambda (_req) t) .
+            eldoc-server--404))
          eldoc-server-port
          nil
          :host eldoc-server-host))
   (message "[eldoc-server] Listening at http://%s:%s/"
            eldoc-server-host
            eldoc-server-port))
+
+;; `httpd-etag'
+(defun eldoc-server--etag (file)
+  "Compute the ETag for FILE."
+  (concat "\"" (substring (sha1 (prin1-to-string (file-attributes file))) -16)
+          "\""))
+
+(defun eldoc-server--send-file (req path type)
+  (with-slots (headers process) req
+    (let ((req-etag (alist-get :IF-NONE-MATCH headers))
+          (etag (eldoc-server--etag path)))
+      (if (equal req-etag etag)
+          (ws-response-header process 304)
+        (with-temp-buffer
+          (set-buffer-multibyte nil)
+          (insert-file-contents-literally path)
+          (ws-response-header
+           process 200
+           (cons "content-type" type)
+           (cons "content-length" (buffer-size))
+           ;; TODO Add Last-Modified
+           ;; TODO Add Date (for ALL responses)
+           (cons "etag" etag))
+          (process-send-region process (point-min) (point-max)))))))
+
+(defun eldoc-server--index (req)
+  (eldoc-server--send-file
+   req
+   (expand-file-name "index.html" eldoc-server--load-dir)
+   "text/html; charset=utf-8"))
+
+(defun eldoc-server--404 (req)
+  (let ((proc (oref req process))
+        (body "<h1>404 Not Found</h1>"))
+    (ws-response-header
+     proc 404
+     '("content-type" . "text/html; charset=utf-8")
+     `("content-length" . ,(string-bytes body)))
+    (process-send-string proc body)))
 
 (defun eldoc-server--api-describe-symbol (req)
   (let ((headers (oref req headers))
@@ -69,18 +117,29 @@
     (let ((symbol (assoc-default "symbol" headers)))
       (pcase symbol
         ('nil 
-         (eldoc-server--end proc 400 '((error . "Missing the symbol parameter"))))
+         (eldoc-server--end
+          proc 400
+          '((error . "Missing the symbol parameter"))))
         (""
-         (eldoc-server--end proc 400 '((error . "Missing value for the symbol parameter"))))
+         (eldoc-server--end
+          proc 400
+          '((error . "Missing value for the symbol parameter"))))
         ((guard (null (intern-soft symbol)))
-         (eldoc-server--end proc 400 `((error . ,(format "The symbol named %S is not defined in this Emacs instance"
-                                                         symbol)))))
+         (eldoc-server--end
+          proc 400
+          `((error . ,(format
+                       "The symbol named %S is not defined in this Emacs instance"
+                       symbol)))))
         (_
          (and (get-buffer "*Help*") (kill-buffer "*Help*"))
-         (describe-symbol (intern-soft symbol))       ; timeout, error
+         (describe-symbol (intern-soft symbol)) ; timeout, error
          (let ((docstring (with-current-buffer "*Help*"
                             (buffer-substring-no-properties (point-min) (point-max)))))
            (and (get-buffer "*Help*") (kill-buffer "*Help*"))
+           (if (string= "" docstring)
+               (eldoc-server--end
+                proc 400
+                `((error . ,(format "No documentation for %s" symbol)))))
            (eldoc-server--end proc 200 `((result . ,docstring)))))))))
 
 (provide 'eldoc-server)
